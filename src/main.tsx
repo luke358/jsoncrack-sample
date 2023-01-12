681d60182ab179f6790258d8a9259b8e0d8f7d28
zhuxuechang <1494135711@qq.com>
2023-01-12 15:24:12 +0800

    feat: global style

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ThemeProvider } from 'styled-components'
 import App from './App'
+import GlobalStyle from './constants/globalStyle';
 import { darkTheme, lightTheme } from './constants/theme';
 import useStored from './store/useStored';
 // import './index.css'
@@ -10,9 +11,10 @@ import useStored from './store/useStored';
 function JSONTrack() {
   const lightmode = useStored(state => state.lightmode);
   return <ThemeProvider theme={lightmode ? lightTheme : darkTheme}>
+    <GlobalStyle />
     <App />
   </ThemeProvider>
 }
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <JSONTrack />
+  <JSONTrack />
 )
